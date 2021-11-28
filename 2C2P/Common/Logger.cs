809033ffb8223e427ccbd2ad4364b0d6045afc61
bharat.thanki <bharat.thanki@randstad.com.my>
2021-11-28 22:41:32 +0800

    logger

--- 2C2P/Common/Logger.cs
+++ 2C2P/Common/Logger.cs
@@ -7,29 +7,45 @@ namespace _2C2P.Common
 {
     public class Logger : ILogger
     {
-
         public void LogInformation(string message)
         {
-            _telemetryClient.TrackTrace(message, SeverityLevel.Information, _commonProperties);
+
         }
 
+        // Log error message
         public void LogError(string message)
         {
-            _telemetryClient.TrackTrace(message, SeverityLevel.Error, _commonProperties);
+
         }
 
+        // Log metric and its value
         public void LogMetric(string metric, double value)
         {
-            _telemetryClient.GetMetric(metric).TrackValue(value);
-        }
-
-        private IDictionary<string, string> GenerateCommonProperties()
-        {
-            return new Dictionary<string, string>()
-                {
-                    {"Environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") }
-                };
 
         }
+
+        /* public void LogInformation(string message)
+         {
+             _telemetryClient.TrackTrace(message, SeverityLevel.Information, _commonProperties);
+         }
+
+         public void LogError(string message)
+         {
+             _telemetryClient.TrackTrace(message, SeverityLevel.Error, _commonProperties);
+         }
+
+         public void LogMetric(string metric, double value)
+         {
+             _telemetryClient.GetMetric(metric).TrackValue(value);
+         }
+
+         private IDictionary<string, string> GenerateCommonProperties()
+         {
+             return new Dictionary<string, string>()
+                 {
+                     {"Environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") }
+                 };
+
+         }*/
     }
 }
